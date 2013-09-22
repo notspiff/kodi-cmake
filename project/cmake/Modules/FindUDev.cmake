97470faaa0acaef7d75075c3322b52460c9f0d7b
spiff <spiff@xbmc.org>
2013-09-22 06:00:03 +0200

    cmake buildsystem

---
+++ project/cmake/Modules/FindUDev.cmake
@@ -0,0 +1,20 @@
+# - Try to find udev
+# Once done this will define
+#
+# UDEV_FOUND - system has libudev 
+# UDEV_INCLUDE_DIRS - the libudev include directory
+# UDEV_LIBRARIES - The libudev libraries
+
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules (UDEV libudev)
+  list(APPEND UDEV_INCLUDE_DIRS ${UDEV_INCLUDEDIR})
+else()
+  find_path(UDEV_INCLUDE_DIRS libudev.h)
+  find_library(UDEV_LIBRARIES udev)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(UDEV DEFAULT_MSG UDEV_INCLUDE_DIRS UDEV_LIBRARIES)
+
+mark_as_advanced(UDEV_INCLUDE_DIRS UDEV_LIBRARIES)
+list(APPEND UDEV_DEFINITIONS -DUSE_LIBUDEV=1)
