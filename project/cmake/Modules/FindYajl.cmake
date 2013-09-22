e5287ce766fd55155f667261a71121634aa849c8
wsnipex <wsnipex@a1.net>
2013-09-22 13:17:34 +0200

    fix building with yajl2

--- project/cmake/Modules/FindYajl.cmake
+++ project/cmake/Modules/FindYajl.cmake
@@ -3,8 +3,23 @@
 #
 # Find libyajl
  
-find_path(YAJL_INCLUDE_DIRS yajl/yajl_common.h)
-find_library(YAJL_LIBRARIES NAMES yajl)
+
+pkg_check_modules(YAJL yajl>=2.0)
+if(YAJL_FOUND)
+  list(APPEND YAJL_DEFINITIONS -DYAJL_MAJOR=2)
+else()
+  find_path(YAJL_INCLUDE_DIRS yajl/yajl_common.h)
+  find_library(YAJL_LIBRARIES NAMES yajl)
+endif()
  
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(YAJL DEFAULT_MSG YAJL_INCLUDE_DIRS YAJL_LIBRARIES)
+
+#if(YAJL_FOUND)
+#  find_path(YAJL_VERSION_INCLUDE yajl/yajl_version.h)
+#  if(YAJL_VERSION_INCLUDE)
+#    list(APPEND YAJL_DEFINITIONS -DYAJL_MAJOR=1)
+#  endif()
+#endif()
+#mark_as_advanced(YAJL_INCLUDE_DIRS YAJL_LIBRARIES YAJL_DEFINITIONS)
+
