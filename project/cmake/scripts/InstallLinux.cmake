4c8d58a6eb6c65e569054b1bf5ccd1cd9c9ec258
wsnipex <wsnipex@a1.net>
2013-09-23 15:04:26 +0200

    [install] path setup:  allow overrides and finer grained config

--- project/cmake/scripts/InstallLinux.cmake
+++ project/cmake/scripts/InstallLinux.cmake
@@ -1,48 +1,46 @@
-set(libdir ${CMAKE_INSTALL_PREFIX}/lib)
-set(bindir ${CMAKE_INSTALL_PREFIX}/bin)
 configure_file(${CORE_SOURCE_DIR}/tools/Linux/xbmc.sh.in
                ${CORE_BUILD_DIR}/scripts/xbmc @ONLY)
 configure_file(${CORE_SOURCE_DIR}/tools/Linux/xbmc-standalone.sh.in
                ${CORE_BUILD_DIR}/scripts/xbmc-standalone @ONLY)
 
-install(TARGETS xbmc-xrandr DESTINATION lib/xbmc)
-install(FILES ${addon_bindings} DESTINATION include/xbmc)
+install(TARGETS xbmc-xrandr DESTINATION ${libdir}/xbmc)
+install(FILES ${addon_bindings} DESTINATION ${includedir}/xbmc)
 install(FILES ${cmake_files} ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/modules/xbmc-config.cmake
-        DESTINATION lib/xbmc)
+        DESTINATION ${libdir}/xbmc)
 install(PROGRAMS ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/scripts/xbmc
                  ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/scripts/xbmc-standalone
-        DESTINATION bin)
+        DESTINATION ${bindir})
 install(FILES ${CORE_SOURCE_DIR}/tools/Linux/FEH.py
-        DESTINATION share/xbmc)
+        DESTINATION ${datarootdir}/xbmc)
 
 install(FILES ${CORE_SOURCE_DIR}/tools/Linux/xbmc-xsession.desktop
         RENAME XBMC.desktop
-        DESTINATION share/xsessions)
+        DESTINATION ${datarootdir}/xsessions)
                 
 install(FILES ${CORE_SOURCE_DIR}/LICENSE.GPL
               ${CORE_SOURCE_DIR}/docs/README.linux
-        DESTINATION share/doc/xbmc)
+        DESTINATION ${datarootdir}/doc/xbmc)
 
 install(FILES ${CORE_SOURCE_DIR}/tools/Linux/xbmc.desktop
-        DESTINATION share/applications)
+        DESTINATION ${datarootdir}/applications)
 
 foreach(texture ${XBT_FILES})
   string(REPLACE "${CMAKE_BINARY_DIR}/" "" dir ${texture})
   get_filename_component(dir ${dir} PATH)
   install(FILES ${texture}
-          DESTINATION share/xbmc/${dir})
+          DESTINATION ${datarootdir}/xbmc/${dir})
 endforeach()
 
 foreach(wraplib ${WRAP_FILES})
   get_filename_component(dir ${wraplib} PATH)
   install(PROGRAMS ${CMAKE_BINARY_DIR}/${wraplib}
-          DESTINATION lib/xbmc/${dir})
+          DESTINATION ${libdir}/xbmc/${dir})
 endforeach()
 
 foreach(file ${install_data})
   get_filename_component(dir ${file} PATH)
   install(FILES ${CMAKE_BINARY_DIR}/${file}
-          DESTINATION share/xbmc/${dir})
+          DESTINATION ${datarootdir}/xbmc/${dir})
 endforeach()
 
 install(CODE "file(STRINGS ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/extra-installs dirs)
@@ -50,7 +48,7 @@ install(CODE "file(STRINGS ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/extra-installs 
                 file(GLOB_RECURSE FILES RELATIVE ${CMAKE_BINARY_DIR} \${dir}/*)
                 foreach(file \${FILES})
                   get_filename_component(dir \${file} PATH)
-                  file(INSTALL \${file} DESTINATION ${CMAKE_INSTALL_PREFIX}/share/xbmc/\${dir})
+                  file(INSTALL \${file} DESTINATION ${datarootdir}/xbmc/\${dir})
                 endforeach()
               endforeach()")
 foreach(subdir ${build_dirs})
@@ -61,9 +59,9 @@ endforeach()
 
 install(FILES ${CORE_SOURCE_DIR}/tools/Linux/xbmc-48x48.png
         RENAME xbmc.png
-        DESTINATION share/icons/hicolor/48x48/apps)
+        DESTINATION ${datarootdir}/icons/hicolor/48x48/apps)
 install(FILES ${CORE_SOURCE_DIR}/media/icon.png
         RENAME xbmc.png
-        DESTINATION share/icons/hicolor/256x256/apps)
+        DESTINATION ${datarootdir}/icons/hicolor/256x256/apps)
 
-install(CODE "execute_process(COMMAND gtk-update-icon-cache -f -q -t $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/share/icons/hicolor ERROR_QUIET)")
+install(CODE "execute_process(COMMAND gtk-update-icon-cache -f -q -t $ENV{DESTDIR}${datarootdir}/icons/hicolor ERROR_QUIET)")
