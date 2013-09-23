10d22e6f2047ae1847404e3a60aff972241641e4
Arne Morten Kvarving <cptspiff@gmail.com>
2013-09-23 13:03:57 +0200

    changed: build with a prefix to avoid cluttering the build dir
    some peeps still do in-source builds out of old habits. this is
    to annoy them slightly less, though they should STOP doing in-source
    builds in the first place

--- project/cmake/Modules/FindLibDvd.cmake
+++ project/cmake/Modules/FindLibDvd.cmake
@@ -1,6 +1,6 @@
 if(ENABLE_DVDCSS)
   ExternalProject_ADD(dvdcss SOURCE_DIR ${XBMC_SOURCE_DIR}/lib/libdvd/libdvdcss/
-                      PREFIX libdvd
+                      PREFIX ${XBMC_BUILD_DIR}/libdvd
                       UPDATE_COMMAND autoreconf -vif
                       CONFIGURE_COMMAND <SOURCE_DIR>/configure
                                         --disable-doc
@@ -9,17 +9,17 @@ if(ENABLE_DVDCSS)
                                         --with-pic
                                         --prefix=<INSTALL_DIR>)
 
-  xbmc_link_library(${CMAKE_BINARY_DIR}/libdvd/lib/libdvdcss.a
+  xbmc_link_library(${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/libdvd/lib/libdvdcss.a
                     system/players/dvdplayer/libdvdcss dvdcss)
 endif()
 
 set(DVDREAD_CFLAGS "-D_XBMC")
 if(ENABLE_DVDCSS)
-  set(DVDREAD_CFLAGS "${DVDREAD_CFLAGS} -DHAVE_DVDCSS_DVDCSS_H -I${CMAKE_BINARY_DIR}/libdvd/include")
+  set(DVDREAD_CFLAGS "${DVDREAD_CFLAGS} -DHAVE_DVDCSS_DVDCSS_H -I${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/libdvd/include")
 endif(ENABLE_DVDCSS)
 
 ExternalProject_ADD(dvdread SOURCE_DIR ${XBMC_SOURCE_DIR}/lib/libdvd/libdvdread/
-                    PREFIX libdvd
+                    PREFIX ${XBMC_BUILD_DIR}/libdvd
                     UPDATE_COMMAND autoreconf -vif
                     CONFIGURE_COMMAND <SOURCE_DIR>/configure
                                       --enable-static
@@ -31,27 +31,23 @@ if(ENABLE_DVDCSS)
   add_dependencies(dvdread dvdcss)
 endif()
 
-xbmc_link_library(${CMAKE_BINARY_DIR}/libdvd/lib/libdvdread.a
+xbmc_link_library(${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/libdvd/lib/libdvdread.a
                   system/players/dvdplayer/libdvdread dvdread)
 
-set(dvdnav_conf )
-string(REPLACE ";" " " dvdnav_conf "${dvdnav_conf}")
-file(WRITE ${CMAKE_BINARY_DIR}/libdvd/tmp/configure_dvdnav ${dvdnav_conf})
-execute_process(COMMAND chmod 755 ${CMAKE_BINARY_DIR}/libdvd/tmp/configure_dvdnav)
 ExternalProject_ADD(dvdnav SOURCE_DIR ${XBMC_SOURCE_DIR}/lib/libdvd/libdvdnav/
-                    PREFIX libdvd
+                    PREFIX ${XBMC_BUILD_DIR}/libdvd
                     UPDATE_COMMAND autoreconf -vif
                     CONFIGURE_COMMAND <SOURCE_DIR>/configure
                                       --disable-shared
                                       --enable-static
                                       --with-pic
-                                      --prefix=${CMAKE_BINARY_DIR}/libdvd
-                                      --with-dvdread-config=${CMAKE_BINARY_DIR}/libdvd/bin/dvdread-config
-                                      "LDFLAGS=-L${CMAKE_BINARY_DIR}/libdvd/lib"
+                                      --prefix=${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/libdvd
+                                      --with-dvdread-config=${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/libdvd/bin/dvdread-config
+                                      "LDFLAGS=-L${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/libdvd/lib"
                                       "CFLAGS=${DVDREAD_CFLAGS}"
                                       "LIBS=-ldvdcss")
 add_dependencies(dvdnav dvdread)
-xbmc_link_library(${CMAKE_BINARY_DIR}/libdvd/lib/libdvdnav.a
+xbmc_link_library(${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/libdvd/lib/libdvdnav.a
                   system/players/dvdplayer/libdvdnav dvdnav)
 
 set(WRAP_FILES ${WRAP_FILES} PARENT_SCOPE)
