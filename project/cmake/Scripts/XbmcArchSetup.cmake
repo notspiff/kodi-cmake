174b4f823b0f54d13a14df97be6820e48aeafa97
wsnipex <wsnipex@a1.net>
2013-09-23 14:18:47 +0200

    [archsetup] detect i386 and allow overwrite of arch and cpu

--- project/cmake/Scripts/XbmcArchSetup.cmake
+++ project/cmake/Scripts/XbmcArchSetup.cmake
@@ -1,12 +1,32 @@
+message(STATUS "system type: ${CMAKE_SYSTEM_NAME}")
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
   set(ARCH_DEFINES -D_LINUX -DTARGET_POSIX -DTARGET_LINUX)
-  if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
-    set(ARCH x86_64-linux)
-  endif()
   set(SYSTEM_DEFINES -D__STDC_CONSTANT_MACROS -D_FILE_DEFINED
                      -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64)
   set(DEP_DEFINES -DLIRC_DEVICE=${LIRC_DEVICE})
+
+  if(WITH_CPU)
+    set(CPU ${WITH_CPU})
+  else()
+    set(CPU ${CMAKE_SYSTEM_PROCESSOR})
+  endif()
+
+  if(WITH_ARCH)
+    set(ARCH ${WITH_ARCH})
+  else()
+    if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+      set(ARCH x86_64-linux)
+    elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "i.86")
+      set(ARCH i486-linux)
+    else()
+      message(WARNING "unknown CPU: ${CPU}")
+    endif()
+  endif()
+
+else()
+  message(WARNING "unsupported system")
 endif()
+message(STATUS "CPU: ${CPU}, ARCH: ${ARCH}")
 
 include(CheckCXXSourceCompiles)
 
