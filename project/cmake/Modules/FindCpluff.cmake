9400bf3f1205993fc84ba63356705035327bca03
spiff <spiff@xbmc.org>
2013-09-24 20:34:41 +0200

    changed: replace xbmc_ prefix with core_ prefix
    improves reusability

--- project/cmake/Modules/FindCpluff.cmake
+++ project/cmake/Modules/FindCpluff.cmake
@@ -1,8 +1,8 @@
 string(REPLACE ";" " " defines "${SYSTEM_DEFINES}")
-ExternalProject_ADD(libcpluff SOURCE_DIR ${XBMC_SOURCE_DIR}/lib/cpluff
-                    PREFIX ${XBMC_BUILD_DIR}/cpluff
+ExternalProject_ADD(libcpluff SOURCE_DIR ${CORE_SOURCE_DIR}/lib/cpluff
+                    PREFIX ${CORE_BUILD_DIR}/cpluff
                     UPDATE_COMMAND ${UPDATE_COMMAND}
-                    CONFIGURE_COMMAND ${XBMC_SOURCE_DIR}/lib/cpluff/configure
+                    CONFIGURE_COMMAND ${CORE_SOURCE_DIR}/lib/cpluff/configure
                                       --disable-nls
                                       --enable-static
                                       --disable-shared
@@ -10,11 +10,11 @@ ExternalProject_ADD(libcpluff SOURCE_DIR ${XBMC_SOURCE_DIR}/lib/cpluff
                                       --prefix=<INSTALL_DIR>
                                       CFLAGS=${defines})
 
-set(CPLUFF_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/cpluff/include)
+set(CPLUFF_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/cpluff/include)
 set(CPLUFF_FOUND 1)
 
-xbmc_link_library(${CMAKE_BINARY_DIR}/${XBMC_BUILD_DIR}/cpluff/lib/libcpluff.a
-                 system/libcpluff libcpluff "extras" -lexpat)
+core_link_library(${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/cpluff/lib/libcpluff.a
+                  system/libcpluff libcpluff "extras" -lexpat)
 set(WRAP_FILES ${WRAP_FILES} PARENT_SCOPE)
 
 mark_as_advanced(CPLUFF_INCLUDE_DIRS CPLUFF_FOUND)
