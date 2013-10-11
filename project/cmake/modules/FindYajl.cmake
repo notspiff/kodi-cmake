ec0d016161a9acac78bd269996ffeba789834f29
Arne Morten Kvarving <cptspiff@gmail.com>
2013-10-11 10:12:45 +0200

    attempt to clean up yajl detection

--- project/cmake/modules/FindYajl.cmake
+++ project/cmake/modules/FindYajl.cmake
@@ -3,23 +3,22 @@
 #
 # Find libyajl
  
-
 pkg_check_modules(YAJL yajl>=2.0)
 if(YAJL_FOUND)
   list(APPEND YAJL_DEFINITIONS -DYAJL_MAJOR=2)
-else()
+endif()
+
+if(NOT YAJL_FOUND)
   find_path(YAJL_INCLUDE_DIRS yajl/yajl_common.h)
   find_library(YAJL_LIBRARIES NAMES yajl)
+
+  file(STRINGS ${YAJL_INCLUDE_DIRS}/yajl/yajl_version.h version_header)
+  string(REGEX MATCH "YAJL_MAJOR ([0-9]+)" YAJL_VERSION_MAJOR ${version_header})
+  string(REGEX REPLACE "YAJL_MAJOR ([0-9]+)" "\\1" YAJL_VERSION_MAJOR "${YAJL_VERSION_MAJOR}")
+  list(APPEND YAJL_DEFINITIONS -DYAJL_MAJOR=${YAJL_VERSION_MAJOR})
 endif()
  
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Yajl DEFAULT_MSG YAJL_INCLUDE_DIRS YAJL_LIBRARIES)
 
-#if(YAJL_FOUND)
-#  find_path(YAJL_VERSION_INCLUDE yajl/yajl_version.h)
-#  if(YAJL_VERSION_INCLUDE)
-#    list(APPEND YAJL_DEFINITIONS -DYAJL_MAJOR=1)
-#  endif()
-#endif()
-#mark_as_advanced(YAJL_INCLUDE_DIRS YAJL_LIBRARIES YAJL_DEFINITIONS)
-
+mark_as_advanced(YAJL_INCLUDE_DIRS YAJL_LIBRARIES YAJL_DEFINITIONS)
