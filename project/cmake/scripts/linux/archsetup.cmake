0f1f749e801cd0801f970b47632ef6518f5c009f
spiff <spiff@xbmc.org>
2013-10-13 12:34:35 +0200

    restructure scripts

---
+++ project/cmake/scripts/linux/archsetup.cmake
@@ -0,0 +1,17 @@
+set(ARCH_DEFINES -D_LINUX -DTARGET_POSIX -DTARGET_LINUX)
+set(SYSTEM_DEFINES -D__STDC_CONSTANT_MACROS -D_FILE_DEFINED
+                   -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64)
+if(WITH_ARCH)
+  set(ARCH ${WITH_ARCH})
+else()
+  if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+    set(ARCH x86_64-linux)
+  elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "i.86")
+    set(ARCH i486-linux)
+  else()
+    message(WARNING "unknown CPU: ${CPU}")
+  endif()
+endif()
+
+set(LIRC_DEVICE          "\"/dev/lircd\"" CACHE STRING "LIRC device to use")
+set(DEP_DEFINES -DLIRC_DEVICE=${LIRC_DEVICE})
