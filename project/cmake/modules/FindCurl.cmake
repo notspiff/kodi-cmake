c438aee34785928aed2d61ade639dcac1bb8d9f1
wsnipex <wsnipex@a1.net>
2013-10-13 19:50:36 +0200

    fix curl detection with unified deps - there is no pc file and we cross compiling

--- project/cmake/modules/FindCurl.cmake
+++ project/cmake/modules/FindCurl.cmake
@@ -8,10 +8,11 @@
 if(PKG_CONFIG_FOUND)
   pkg_check_modules (CURL libcurl)
   list(APPEND CURL_INCLUDE_DIRS ${CURL_INCLUDEDIR})
-else()
-  find_package(CURL)
 endif()
 
+if(NOT CURL_FOUND)
+  find_package(CURL)
+endif()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Curl DEFAULT_MSG CURL_INCLUDE_DIRS CURL_LIBRARIES)
 
