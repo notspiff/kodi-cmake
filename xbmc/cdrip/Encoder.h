3919de67a8c205787a8e5d75f27ea0c3280d8773
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-15 20:55:40 +0100

    move vorbis, lame and flac audio encoders to add-ons

--- xbmc/cdrip/Encoder.h
+++ xbmc/cdrip/Encoder.h
@@ -21,7 +21,9 @@
  *
  */
 
-#include "utils/StdString.h"
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <stdint.h>
 
 #define ENC_ARTIST  11
 #define ENC_TITLE   12
@@ -33,73 +35,74 @@
 
 #define WRITEBUFFER_SIZE 131072 // 128k buffer
 
-enum CDDARipEncoder
-{
-  CDDARIP_ENCODER_LAME    = 0,
-  CDDARIP_ENCODER_VORBIS,
-  CDDARIP_ENCODER_WAV,
-  CDDARIP_ENCODER_FLAC,
-  CDDARIP_ENCODER_FFMPEG_M4A,
-  CDDARIP_ENCODER_FFMPEG_WMA
-};
-
-enum CDDARipQuality
-{
-  CDDARIP_QUALITY_CBR      = 0,
-  CDDARIP_QUALITY_MEDIUM,
-  CDDARIP_QUALITY_STANDARD,
-  CDDARIP_QUALITY_EXTREME
-};
-
 namespace XFILE { class CFile; }
+
+class IEncoder
+{
+public:
+  IEncoder()
+  {
+    m_iInChannels = 0;
+    m_iInSampleRate = 0;
+    m_iInBitsPerSample = 0;
+  }
+  virtual ~IEncoder() {}
+  virtual bool Init() = 0;
+  virtual int Encode(int nNumBytesRead, uint8_t* pbtStream,
+                       uint8_t* buffer) = 0;
+  virtual int Flush(uint8_t* buffer) = 0;
+  virtual bool Close() = 0;
+
+  // tag info
+  std::string m_strComment;
+  std::string m_strArtist;
+  std::string m_strAlbumArtist;
+  std::string m_strTitle;
+  std::string m_strAlbum;
+  std::string m_strGenre;
+  std::string m_strTrack;
+  std::string m_strYear;
+  std::string m_strFile;
+  int m_iTrackLength;
+  int m_iInChannels;
+  int m_iInSampleRate;
+  int m_iInBitsPerSample;
+};
 
 class CEncoder
 {
 public:
-  CEncoder();
+  CEncoder(boost::shared_ptr<IEncoder> encoder);
   virtual ~CEncoder();
-  virtual bool Init(const char* strFile, int iInChannels, int iInRate, int iInBits) = 0;
-  virtual int Encode(int nNumBytesRead, uint8_t* pbtStream) = 0;
-  virtual bool Close() = 0;
+  virtual bool Init(const char* strFile, int iInChannels, int iInRate, int iInBits);
+  virtual int Encode(int nNumBytesRead, uint8_t* pbtStream);
+  virtual bool CloseEncode();
 
-  void SetComment(const CStdString& str) { m_strComment = str; }
-  void SetArtist(const CStdString& str) { m_strArtist = str; }
-  void SetTitle(const CStdString& str) { m_strTitle = str; }
-  void SetAlbum(const CStdString& str) { m_strAlbum = str; }
-  void SetAlbumArtist(const CStdString& str) { m_strAlbumArtist = str; }
-  void SetGenre(const CStdString& str) { m_strGenre = str; }
-  void SetTrack(const CStdString& str) { m_strTrack = str; }
-  void SetTrackLength(int length) { m_iTrackLength = length; }
-  void SetYear(const CStdString& str) { m_strYear = str; }
+  void SetComment(const std::string& str) { m_impl->m_strComment = str; }
+  void SetArtist(const std::string& str) { m_impl->m_strArtist = str; }
+  void SetTitle(const std::string& str) { m_impl->m_strTitle = str; }
+  void SetAlbum(const std::string& str) { m_impl->m_strAlbum = str; }
+  void SetAlbumArtist(const std::string& str) { m_impl->m_strAlbumArtist = str; }
+  void SetGenre(const std::string& str) { m_impl->m_strGenre = str; }
+  void SetTrack(const std::string& str) { m_impl->m_strTrack = str; }
+  void SetTrackLength(int length) { m_impl->m_iTrackLength = length; }
+  void SetYear(const std::string& str) { m_impl->m_strYear = str; }
 
-protected:
   bool FileCreate(const char* filename);
   bool FileClose();
   int FileWrite(const void *pBuffer, uint32_t iBytes);
   int64_t FileSeek(int64_t iFilePosition, int iWhence = SEEK_SET);
+protected:
 
   int WriteStream(const void *pBuffer, uint32_t iBytes);
   int FlushStream();
 
-  // tag info
-  CStdString m_strComment;
-  CStdString m_strArtist;
-  CStdString m_strAlbumArtist;
-  CStdString m_strTitle;
-  CStdString m_strAlbum;
-  CStdString m_strGenre;
-  CStdString m_strTrack;
-  CStdString m_strYear;
-  int m_iTrackLength;
-
-  CStdString m_strFile;
+  boost::shared_ptr<IEncoder> m_impl;
 
   XFILE::CFile *m_file;
-  int m_iInChannels;
-  int m_iInSampleRate;
-  int m_iInBitsPerSample;
 
   uint8_t m_btWriteBuffer[WRITEBUFFER_SIZE]; // 128k buffer for writing to disc
+  uint8_t m_buffer[WRITEBUFFER_SIZE*3/2]; // encode buffer - data may occasionally grow
   uint32_t m_dwWriteBufferPointer;
 };
 
