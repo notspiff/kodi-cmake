700547a3a1ef93780e51b94528b93ac559e49e3c
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-16 00:20:24 +0100

    add support for audio decoder addons

--- xbmc/addons/Addon.cpp
+++ xbmc/addons/Addon.cpp
@@ -88,6 +88,7 @@ static const TypeMapping types[] =
    {"xbmc.addon.image",                  ADDON_IMAGE,                1039, "DefaultAddonPicture.png" },
    {"xbmc.addon.executable",             ADDON_EXECUTABLE,           1043, "DefaultAddonProgram.png" },
    {"xbmc.audioencoder",                 ADDON_AUDIOENCODER,         200,  "DefaultAddonAudioEncoder.png" },
+   {"xbmc.audiodecoder",                 ADDON_AUDIODECODER,         201,  "DefaultAddonAudioDecoder.png" },
    {"xbmc.service",                      ADDON_SERVICE,             24018, "DefaultAddonService.png" }};
 
 const CStdString TranslateType(const ADDON::TYPE &type, bool pretty/*=false*/)
@@ -391,6 +392,7 @@ void CAddon::BuildLibName(const cp_extension_t *extension)
       case ADDON_PLUGIN:
       case ADDON_SERVICE:
       case ADDON_REPOSITORY:
+      case ADDON_AUDIODECODER:
       case ADDON_AUDIOENCODER:
         {
           CStdString temp = CAddonMgr::Get().GetExtValue(extension->configuration, "@library");
