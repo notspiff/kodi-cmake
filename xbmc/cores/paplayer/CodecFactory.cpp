da8cecb7b4b82b7b7fed7b247dd0eaf94051cf52
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-18 15:49:17 +0100

    remove vgmstream
    now an add-on

--- xbmc/cores/paplayer/CodecFactory.cpp
+++ xbmc/cores/paplayer/CodecFactory.cpp
@@ -77,8 +77,6 @@ ICodec* CodecFactory::CreateCodec(const CStdString& strFileType)
   else if (strFileType.Equals("spc"))
     return new SPCCodec();
 #endif
-  else if (VGMCodec::IsSupportedFormat(strFileType))
-    return new VGMCodec();
   else if (strFileType.Equals("wma"))
     return new DVDPlayerCodec();
   else if (strFileType.Equals("aiff") || strFileType.Equals("aif"))
