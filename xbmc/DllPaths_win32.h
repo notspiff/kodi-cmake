897c5addebd56617c72ce7ff6c0f28c5495f287b
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-18 15:49:17 +0100

    remove vgmstream
    now an add-on

--- xbmc/DllPaths_win32.h
+++ xbmc/DllPaths_win32.h
@@ -39,7 +39,6 @@
 #define DLL_PATH_ASAP_CODEC    "special://xbmcbin/system/players/paplayer/xbmc_asap.dll"
 #define DLL_PATH_MID_CODEC     "special://xbmcbin/system/players/paplayer/timidity.dll"
 #define DLL_PATH_SPC_CODEC     "special://xbmcbin/system/players/paplayer/SNESAPU.dll"
-#define DLL_PATH_VGM_CODEC     "special://xbmcbin/system/players/paplayer/vgmstream.dll"
 
 /* dvdplayer */
 #define DLL_PATH_LIBASS        "special://xbmcbin/system/players/dvdplayer/libass.dll"
