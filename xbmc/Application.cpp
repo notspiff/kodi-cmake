2094b536a6fce58c9b6b219d4ec4ef56114d8e13
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-23 16:13:59 +0100

    remove NFS support
    now an add-on

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -120,9 +120,6 @@
 #if defined(TARGET_POSIX) && defined(HAS_FILESYSTEM_SMB)
 #include "filesystem/SMBDirectory.h"
 #endif
-#ifdef HAS_FILESYSTEM_NFS
-#include "filesystem/NFSFile.h"
-#endif
 #ifdef HAS_FILESYSTEM_AFP
 #include "filesystem/AFPFile.h"
 #endif
@@ -4512,10 +4509,6 @@ void CApplication::ProcessSlow()
   smb.CheckIfIdle();
 #endif
 
-#ifdef HAS_FILESYSTEM_NFS
-  gNfsConnection.CheckIfIdle();
-#endif
-
 #ifdef HAS_FILESYSTEM_AFP
   gAfpConnection.CheckIfIdle();
 #endif
@@ -5183,10 +5176,6 @@ void CApplication::CloseNetworkShares()
 
 #if defined(HAS_FILESYSTEM_SMB) && !defined(TARGET_WINDOWS)
   smb.Deinit();
-#endif
-  
-#ifdef HAS_FILESYSTEM_NFS
-  gNfsConnection.Deinit();
 #endif
   
 #ifdef HAS_FILESYSTEM_AFP
