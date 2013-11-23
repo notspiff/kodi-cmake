c8a87f627ca0e5054b89d0ef96ec62812c8410d3
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-23 16:13:59 +0100

    remove NFS support
    now an add-on

--- xbmc/filesystem/DirectoryFactory.cpp
+++ xbmc/filesystem/DirectoryFactory.cpp
@@ -103,9 +103,6 @@
 #ifdef HAS_ZEROCONF
 #include "ZeroconfDirectory.h"
 #endif
-#ifdef HAS_FILESYSTEM_NFS
-#include "NFSDirectory.h"
-#endif
 #ifdef HAS_FILESYSTEM_AFP
 #include "AFPDirectory.h"
 #endif
@@ -230,9 +227,6 @@ IDirectory* CDirectoryFactory::Create(const CURL& url)
 #endif
 #ifdef HAS_ZEROCONF
     if (url.IsProtocol("zeroconf")) return new CZeroconfDirectory();
-#endif
-#ifdef HAS_FILESYSTEM_NFS
-    if (url.IsProtocol("nfs")) return new CNFSDirectory();
 #endif
 #ifdef HAS_FILESYSTEM_AFP
       if (url.IsProtocol("afp")) return new CAFPDirectory();
