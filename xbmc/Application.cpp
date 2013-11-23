7d75830a52d1c85fd151e6f4cbd1bb5ad192a093
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-23 16:13:59 +0100

    remove NFS support
    now an add-on

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4289,10 +4289,6 @@ void CApplication::ProcessSlow()
   smb.CheckIfIdle();
 #endif
 
-#ifdef HAS_FILESYSTEM_NFS
-  gNfsConnection.CheckIfIdle();
-#endif
-
   CVFSEntryManager::Get().ClearOutIdle();
 
   g_mediaManager.ProcessEvents();
@@ -4917,9 +4913,6 @@ void CApplication::CloseNetworkShares()
   smb.Deinit();
 #endif
   
-#ifdef HAS_FILESYSTEM_NFS
-  gNfsConnection.Deinit();
-#endif
   CVFSEntryManager::Get().DisconnectAll();
 }
 
--- xbmc/system.h
+++ xbmc/system.h
@@ -59,10 +59,6 @@
   #define HAS_FILESYSTEM_SMB
 #endif
 
-#ifdef HAVE_LIBNFS
-  #define HAS_FILESYSTEM_NFS
-#endif
-
 #ifdef HAVE_LIBPLIST
   #define HAS_AIRPLAY
 #endif
