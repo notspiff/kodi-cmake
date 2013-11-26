77bf48903a6b44c651b275f56aa81dee10fc6b42
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-26 23:20:14 +0100

    add URLEncode add-on callback

--- addons/library.xbmc.addon/libXBMC_addon.h
+++ addons/library.xbmc.addon/libXBMC_addon.h
@@ -186,6 +186,10 @@ namespace ADDON
         dlsym(m_libXBMC_addon, "XBMC_dns_lookup");
       if (XBMC_dns_lookup == NULL) { fprintf(stderr, "Unable to assign function %s\n", dlerror()); return false; }
 
+      XBMC_url_encode = (char* (*)(void* HANDLE, void* CB, const char* url))
+        dlsym(m_libXBMC_addon, "XBMC_url_encode");
+      if (XBMC_url_encode == NULL) { fprintf(stderr, "Unable to assign function %s\n", dlerror()); return false; }
+
       XBMC_open_file = (void* (*)(void* HANDLE, void* CB, const char* strFileName, unsigned int flags))
         dlsym(m_libXBMC_addon, "XBMC_open_file");
       if (XBMC_open_file == NULL) { fprintf(stderr, "Unable to assign function %s\n", dlerror()); return false; }
@@ -345,6 +349,16 @@ namespace ADDON
     char* DNSLookup(const char* url)
     {
       return XBMC_dns_lookup(m_Handle, m_Callbacks, url);
+    }
+
+    /*!
+     * @brief Encode an URL
+     * @param dwCode The code of the message to get.
+     * @return The message. Must be freed by calling FreeString() when done.
+     */
+    char* URLEncode(const char* url)
+    {
+      return XBMC_url_encode(m_Handle, m_Callbacks, url);
     }
 
     /*!
@@ -581,6 +595,7 @@ namespace ADDON
     char* (*XBMC_get_localized_string)(void *HANDLE, void* CB, int dwCode);
     char* (*XBMC_get_dvd_menu_language)(void *HANDLE, void* CB);
     char* (*XBMC_dns_lookup)(void *HANDLE, void* CB, const char* url);
+    char* (*XBMC_url_encode)(void *HANDLE, void* CB, const char* url);
     void (*XBMC_free_string)(void *HANDLE, void* CB, char* str);
     void* (*XBMC_open_file)(void *HANDLE, void* CB, const char* strFileName, unsigned int flags);
     void* (*XBMC_open_file_for_write)(void *HANDLE, void* CB, const char* strFileName, bool bOverWrite);
