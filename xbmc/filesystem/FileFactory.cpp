1630fa2984d1e167362e2816d7dc5bd281e790d3
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-27 17:45:37 +0100

    remove slingbox
    now an (untested) add-on

--- xbmc/filesystem/FileFactory.cpp
+++ xbmc/filesystem/FileFactory.cpp
@@ -87,7 +87,6 @@
 #include "UDFFile.h"
 #include "MythFile.h"
 #include "HDHomeRunFile.h"
-#include "SlingboxFile.h"
 #include "ImageFile.h"
 #include "Application.h"
 #include "URL.h"
@@ -171,7 +170,6 @@ IFile* CFileFactory::CreateLoader(const CURL& url)
     else if (url.IsProtocol("shout")) return new CShoutcastFile();
     else if (url.IsProtocol("tuxbox")) return new CTuxBoxFile();
     else if (url.IsProtocol("hdhomerun")) return new CHomeRunFile();
-    else if (url.IsProtocol("sling")) return new CSlingboxFile();
     else if (url.IsProtocol("myth")) return new CMythFile();
     else if (url.IsProtocol("cmyth")) return new CMythFile();
 #ifdef HAS_FILESYSTEM_SMB
