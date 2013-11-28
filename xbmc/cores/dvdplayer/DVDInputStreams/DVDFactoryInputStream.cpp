0f08af0078eca7752bf0e969e550f3503d32ef42
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-28 13:30:21 +0100

    remove VTP support
    now an (untested) add-on

--- xbmc/cores/dvdplayer/DVDInputStreams/DVDFactoryInputStream.cpp
+++ xbmc/cores/dvdplayer/DVDInputStreams/DVDFactoryInputStream.cpp
@@ -114,7 +114,6 @@ CDVDInputStream* CDVDFactoryInputStream::CreateInputStream(IDVDPlayer* pPlayer, 
   else if(file.substr(0, 7) == "myth://"
        || file.substr(0, 8) == "cmyth://"
        || file.substr(0, 8) == "gmyth://"
-       || file.substr(0, 6) == "vtp://")
     return new CDVDInputStreamTV();
 #ifdef ENABLE_DVDINPUTSTREAM_STACK
   else if(file.substr(0, 8) == "stack://")
