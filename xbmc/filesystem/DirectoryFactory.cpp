2bc34b87744bae1bae253432afc860e6d30c8cf5
Arne Morten Kvarving <cptspiff@gmail.com>
2013-11-28 14:16:15 +0100

    remove RTV support
    now an (untested) addon

--- xbmc/filesystem/DirectoryFactory.cpp
+++ xbmc/filesystem/DirectoryFactory.cpp
@@ -64,9 +64,6 @@
 #include "PluginDirectory.h"
 #ifdef HAS_FILESYSTEM
 #include "ISO9660Directory.h"
-#ifdef HAS_FILESYSTEM_RTV
-#include "RTVDirectory.h"
-#endif
 #ifdef HAS_FILESYSTEM_DAAP
 #include "DAAPDirectory.h"
 #endif
@@ -184,9 +181,6 @@ IDirectory* CDirectoryFactory::Create(const CURL& url)
 #ifdef HAS_FILESYSTEM
 #ifdef HAS_FILESYSTEM_DAAP
     if (url.IsProtocol("daap")) return new CDAAPDirectory();
-#endif
-#ifdef HAS_FILESYSTEM_RTV
-    if (url.IsProtocol("rtv")) return new CRTVDirectory();
 #endif
 #endif
 #ifdef HAS_UPNP
