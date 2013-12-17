b62a45a54e239ac988a4405ae9b4e5a2aedd9909
montellese <montellese@xbmc.org>
2013-12-17 09:47:11 +0100

    filesystem: add CImportDirectory for import:// paths

--- xbmc/filesystem/DirectoryFactory.cpp
+++ xbmc/filesystem/DirectoryFactory.cpp
@@ -34,6 +34,7 @@
 #include "MusicSearchDirectory.h"
 #include "VideoDatabaseDirectory.h"
 #include "FavouritesDirectory.h"
+#include "ImportDirectory.h"
 #include "LibraryDirectory.h"
 #include "AddonsDirectory.h"
 #include "SourcesDirectory.h"
@@ -167,6 +168,7 @@ IDirectory* CDirectoryFactory::Create(const CStdString& strPath)
   if (strProtocol == "musicsearch") return new CMusicSearchDirectory();
   if (strProtocol == "videodb") return new CVideoDatabaseDirectory();
   if (strProtocol == "library") return new CLibraryDirectory();
+  if (strProtocol == "import") return new CImportDirectory();
   if (strProtocol == "favourites") return new CFavouritesDirectory();
   if (strProtocol == "filereader")
     return CDirectoryFactory::Create(url.GetFileName());
