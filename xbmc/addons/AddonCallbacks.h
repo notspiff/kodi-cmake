e6c2efbf87165812c41ac11254b0c26c3263451f
Arne Morten Kvarving <cptspiff@gmail.com>
2013-12-18 15:12:31 +0100

    add AuthenticateURL addon callback

--- xbmc/addons/AddonCallbacks.h
+++ xbmc/addons/AddonCallbacks.h
@@ -25,6 +25,8 @@
 #include "../../addons/library.xbmc.addon/libXBMC_addon.h"
 #include "../../addons/library.xbmc.gui/libXBMC_gui.h"
 
+struct VFSURL;
+
 typedef void (*AddOnLogCallback)(void *addonData, const ADDON::addon_log_t loglevel, const char *msg);
 typedef void (*AddOnQueueNotification)(void *addonData, const ADDON::queue_msg_t type, const char *msg);
 typedef bool (*AddOnWakeOnLan)(const char* mac);
@@ -34,6 +36,7 @@ typedef char* (*AddOnGetLocalizedString)(const void* addonData, long dwCode);
 typedef char* (*AddOnGetDVDMenuLanguage)(const void* addonData);
 typedef char* (*AddOnDNSLookup)(const void* addonData, const char* url);
 typedef char* (*AddOnURLEncode)(const void* addonData, const char* url);
+typedef bool  (*AddOnAuthenticateURL)(const void* addonData, VFSURL* url);
 typedef void (*AddOnFreeString)(const void* addonData, char* str);
 
 typedef void* (*AddOnOpenFile)(const void* addonData, const char* strFileName, unsigned int flags);
@@ -67,6 +70,7 @@ typedef struct CB_AddOn
   AddOnGetDVDMenuLanguage GetDVDMenuLanguage;
   AddOnDNSLookup          DNSLookup;
   AddOnURLEncode          URLEncode;
+  AddOnAuthenticateURL    AuthenticateURL;
   AddOnFreeString         FreeString;
 
   AddOnOpenFile           OpenFile;
