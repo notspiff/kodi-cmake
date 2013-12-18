d2a58695fb0fe54772d29fd00de2e84182e10e13
Arne Morten Kvarving <cptspiff@gmail.com>
2013-12-18 15:12:31 +0100

    add AuthenticateURL addon callback

--- xbmc/addons/AddonCallbacksAddon.cpp
+++ xbmc/addons/AddonCallbacksAddon.cpp
@@ -35,6 +35,8 @@
 #include "utils/XMLUtils.h"
 #include "cores/dvdplayer/DVDCodecs/DVDCodecs.h"
 #include "URL.h"
+#include "PasswordManager.h"
+#include "include/xbmc_vfs_types.h"
 
 using namespace XFILE;
 
@@ -56,6 +58,7 @@ CAddonCallbacksAddon::CAddonCallbacksAddon(CAddon* addon)
   m_callbacks->GetDVDMenuLanguage = GetDVDMenuLanguage;
   m_callbacks->DNSLookup          = DNSLookup;
   m_callbacks->URLEncode          = URLEncode;
+  m_callbacks->AuthenticateURL    = AuthenticateURL;
   m_callbacks->FreeString         = FreeString;
 
   m_callbacks->OpenFile           = OpenFile;
@@ -325,6 +328,24 @@ char* CAddonCallbacksAddon::URLEncode(const void* addonData, const char* url)
   if (!string.empty())
     buffer = strdup(string.c_str());
   return buffer;
+}
+
+bool CAddonCallbacksAddon::AuthenticateURL(const void* addonData, VFSURL* url)
+{
+  CAddonCallbacks* helper = (CAddonCallbacks*) addonData;
+  if (!helper)
+    return false;
+
+  CURL url2(url->url);
+
+  bool result = CPasswordManager::GetInstance().AuthenticateURL(url2);
+  if (result)
+  {
+    url->username = strdup(url2.GetUserName().c_str());
+    url->password = strdup(url2.GetPassWord().c_str());
+  }
+
+  return result;
 }
 
 void CAddonCallbacksAddon::FreeString(const void* addonData, char* str)
