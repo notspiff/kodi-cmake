250fe5d46e961565fff289a6cfeea0aff171d41a
Arne Morten Kvarving <cptspiff@gmail.com>
2014-01-07 16:34:43 +0100

    add IDirectory callbacks for VFS addons

--- xbmc/addons/include/xbmc_vfs_dll.h
+++ xbmc/addons/include/xbmc_vfs_dll.h
@@ -85,7 +85,8 @@ extern "C"
   bool CreateDirectory(VFSURL* url);
 
   //! \copydoc VFSEntry::GetDirectory
-  void* GetDirectory(VFSURL* url, VFSDirEntry** entries, int* num_entries);
+  void* GetDirectory(VFSURL* url, VFSDirEntry** entries, int* num_entries,
+                     VFSCallbacks* callbacks);
 
   //! \copydoc VFSEntry::FreeDirectory
   void FreeDirectory(void* ctx);
