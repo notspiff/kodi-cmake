aa1f702302a8c07f6fba4d9979e3aa8de1a35cb6
Arne Morten Kvarving <cptspiff@gmail.com>
2014-03-22 17:43:30 +0100

    fix copyright statement

--- xbmc/filesystem/M4BFileDirectory.h
+++ xbmc/filesystem/M4BFileDirectory.h
@@ -1,7 +1,6 @@
 #pragma once
 /*
- *      Copyright (C) 2005-2013 Team XBMC
- *      http://xbmc.org
+ *      Copyright (C) 2014 Arne Morten Kvarving 
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
