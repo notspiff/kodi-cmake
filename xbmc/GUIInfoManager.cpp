57c8a948cb5124ba7118125f440a456c56eb734f
Arne Morten Kvarving <cptspiff@gmail.com>
2014-04-16 16:41:31 +0200

    fixed: count audiobooks as other music library content

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -5514,6 +5514,8 @@ bool CGUIInfoManager::GetLibraryBool(int condition)
       if (db.Open())
       {
         m_libraryHasMusic = (db.GetSongsCount() > 0) ? 1 : 0;
+        if (!m_libraryHasMusic)
+          m_libraryHasMusic = (db.GetAudiobookCount() > 0) ? 1 : 0;
         db.Close();
       }
     }
