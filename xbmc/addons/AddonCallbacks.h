5de81eaec9b772b387c749f1191afeaec1e18dc2
Arne Morten Kvarving <cptspiff@gmail.com>
2014-07-03 20:46:44 +0200

    changed: move bindings to a common directory

--- xbmc/addons/AddonCallbacks.h
+++ xbmc/addons/AddonCallbacks.h
@@ -21,10 +21,10 @@
 
 #include <stdint.h>
 
-#include "../../addons/library.kodi.guilib/libKODI_guilib.h"
-#include "../../addons/library.kodi.adsp/libKODI_adsp.h"
-#include "../../addons/library.kodi.audioengine/libKODI_audioengine.h"
-#include "cores/VideoPlayer/DVDDemuxers/DVDDemuxUtils.h"
+#include "addons/bindings/libKODI_guilib.h"
+#include "addons/bindings/libKODI_adsp.h"
+#include "addons/bindings/libKODI_audioengine.h"
+#include "cores/dvdplayer/DVDDemuxers/DVDDemuxUtils.h"
 #include "addons/include/kodi_adsp_types.h"
 #include "addons/include/kodi_audioengine_types.h"
 #include "addons/include/xbmc_pvr_types.h"
