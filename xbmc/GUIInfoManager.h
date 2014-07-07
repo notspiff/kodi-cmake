f021930aaf7aabf2e807c6f615f03f72700dda44
Arne Morten Kvarving <cptspiff@gmail.com>
2014-07-07 19:02:37 +0200

    changed: move TIME_FORMAT enum to StringUtils

--- xbmc/GUIInfoManager.h
+++ xbmc/GUIInfoManager.h
@@ -30,12 +30,12 @@
 #include "guilib/IMsgTargetCallback.h"
 #include "messaging/IMessageTarget.h"
 #include "inttypes.h"
-#include "XBDateTime.h"
 #include "utils/Observer.h"
 #include "utils/Temperature.h"
 #include "interfaces/info/InfoBool.h"
 #include "interfaces/info/SkinVariable.h"
 #include "cores/IPlayer.h"
+#include "utils/StringUtils.h"
 
 #include <list>
 #include <map>
