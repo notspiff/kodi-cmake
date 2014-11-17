5b7c61de616046a4e41f156b884d4db7b84feaa9
Arne Morten Kvarving <cptspiff@gmail.com>
2014-11-17 11:10:00 +0100

    simplify, rebrand

--- project/cmake/scripts/common/generateversionedfiles.cmake
+++ project/cmake/scripts/common/generateversionedfiles.cmake
@@ -5,10 +5,11 @@ configure_file(${CORE_SOURCE_DIR}/addons/xbmc.addon/addon.xml.in
                ${CMAKE_BINARY_DIR}/addons/xbmc.addon/addon.xml @ONLY)
 configure_file(${CORE_SOURCE_DIR}/xbmc/CompileInfo.cpp.in
                ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/xbmc/CompileInfo.cpp @ONLY)
+set(prefix ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR})
 configure_file(${CORE_SOURCE_DIR}/project/cmake/templates/kodi-config.cmake.in
                ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/lib/kodi/kodi-config.cmake @ONLY)
 configure_file(${CORE_SOURCE_DIR}/project/cmake/templates/kodi-addon-helpers.cmake.in
-  ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/lib/kodi/kodi-addon-helpers.cmake @ONLY)
+               ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/lib/kodi/kodi-addon-helpers.cmake @ONLY)
 
 # Backwards compat with deprecation warning
 configure_file(${CORE_SOURCE_DIR}/project/cmake/templates/xbmc-config.cmake.in
