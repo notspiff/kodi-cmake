garbear/xbmc-retroplayer

Joystick library and peripheral add-ons
Thanks to frostworx for fixing the peripheral add-ons build and a1rwulf for
fixing the joystick library build.

xbmc/peripherals/addons/PeripheralAddon.h
@@ -1,0 +1,134 @@
+#pragma once
+/*
+ *      Copyright (C) 2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "addons/AddonDll.h"
+#include "addons/DllPeripheral.h"
+#include "addons/include/xbmc_peripheral_types.h"
+#include "addons/include/xbmc_peripheral_utils.hpp"
+#include "input/joysticks/JoystickTypes.h"
+#include "peripherals/PeripheralTypes.h"
+#include "threads/Thread.h"
+
+#include <boost/shared_ptr.hpp>
+#include <vector>
+
+class CInputPrimitive;
+class IJoystickInputHandler;
+
+namespace PERIPHERALS
+{
+  class CPeripheral;
+
+  class CPeripheralAddon;
+  typedef boost::shared_ptr<CPeripheralAddon> PeripheralAddonPtr;
+  typedef std::vector<PeripheralAddonPtr>     PeripheralAddonVector;
+
+  typedef boost::shared_ptr<ADDON::JoystickFeature>       JoystickFeaturePtr;
+  typedef std::map<JoystickFeatureID, JoystickFeaturePtr> JoystickFeatureMap;
+
+  class CPeripheralAddon : public ADDON::CAddonDll<DllPeripheral, PeripheralAddon, PERIPHERAL_PROPERTIES>
+  {
+  public:
+    CPeripheralAddon(const ADDON::AddonProps& props);
+    CPeripheralAddon(const cp_extension_t *ext);
+    virtual ~CPeripheralAddon(void);
+
+    virtual ADDON::AddonPtr GetRunningInstance(void) const;
+
+    /*!
+     * @brief Initialise the instance of this add-on
+     */
+    ADDON_STATUS Create(void);
+
+    /*!
+     * @brief Destroy the instance of this add-on
+     */
+    void Destroy(void);
+
+    bool         Register(unsigned int peripheralIndex, CPeripheral* peripheral);
+    void         UnregisterRemovedDevices(const PeripheralScanResults &results, std::vector<CPeripheral*>& removedPeripherals);
+    void         GetFeatures(std::vector<PeripheralFeature> &features) const;
+    bool         HasFeature(const PeripheralFeature feature) const;
+    CPeripheral* GetPeripheral(unsigned int index) const;
+    CPeripheral* GetByPath(const std::string &strPath) const;
+    int          GetPeripheralsWithFeature(std::vector<CPeripheral*> &results, const PeripheralFeature feature) const;
+    size_t       GetNumberOfPeripherals(void) const;
+    size_t       GetNumberOfPeripheralsWithId(const int iVendorId, const int iProductId) const;
+    void         GetDirectory(const CStdString &strPath, CFileItemList &items) const;
+
+    /** @name Peripheral add-on methods */
+    //@{
+    bool PerformDeviceScan(PeripheralScanResults &results);
+    bool ProcessEvents(void);
+    //@}
+
+    /** @name Joystick methods */
+    //@{
+    bool GetJoystickFeatures(unsigned int index, JoystickFeatureMap& features);
+    //@}
+
+    static const char          *ToString(PERIPHERAL_ERROR error);
+
+  protected:
+    /*!
+     * @brief Request the API version from the add-on, and check if it's compatible
+     * @return True when compatible, false otherwise.
+     */
+    virtual bool CheckAPIVersion(void);
+
+  private:
+    static JoystickFeatureID    ToJoystickID(JOYSTICK_FEATURE_ID id);
+    static JOYSTICK_FEATURE_ID ToFeatureID(JoystickFeatureID id);
+    static HatDirection        ToHatDirection(JOYSTICK_STATE_HAT state);
+
+    /*!
+     * @brief Resets all class members to their defaults. Called by the constructors
+     */
+    void ResetProperties(void);
+
+    bool GetAddonProperties(void);
+
+    /*!
+     * @brief Checks whether the provided API version is compatible with XBMC
+     * @param minVersion The add-on's XBMC_PERIPHERAL_MIN_API_VERSION version
+     * @param version The add-on's XBMC_PERIPHERAL_API_VERSION version
+     * @return True when compatible, false otherwise
+     */
+    static bool IsCompatibleAPIVersion(const ADDON::AddonVersion &minVersion, const ADDON::AddonVersion &version);
+
+    bool LogError(const PERIPHERAL_ERROR error, const char *strMethod) const;
+    void LogException(const std::exception &e, const char *strFunctionName) const;
+
+    /* stored strings to make sure const char* members in PERIPHERAL_PROPERTIES stay valid */
+    std::string         m_strUserPath;    /*!< @brief translated path to the user profile */
+    std::string         m_strClientPath;  /*!< @brief translated path to this add-on */
+
+    /* add-on properties */
+    ADDON::AddonVersion m_apiVersion;
+    bool                m_bProvidesJoysticks;
+
+    /* peripherals */
+    std::map<unsigned int, CPeripheral*>  m_peripherals;
+
+    /* synchronization */
+    CCriticalSection    m_critSection;
+  };
+}

xbmc/peripherals/bus/PeripheralBusAddon.cpp
@@ -1,0 +1,246 @@
+/*
+ *      Copyright (C) 2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "PeripheralBusAddon.h"
+#include "addons/AddonDatabase.h"
+#include "addons/AddonManager.h"
+#include "peripherals/Peripherals.h"
+#include "threads/SingleLock.h"
+#include "utils/log.h"
+
+#include <algorithm>
+
+using namespace ADDON;
+using namespace PERIPHERALS;
+
+CPeripheralBusAddon::CPeripheralBusAddon(CPeripherals *manager) :
+    CPeripheralBus("PeripBusAddon", manager, PERIPHERAL_BUS_ADDON)
+{
+}
+
+CPeripheralBusAddon::~CPeripheralBusAddon(void)
+{
+}
+
+bool CPeripheralBusAddon::GetAddon(const std::string &strId, AddonPtr &addon) const
+{
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator it = m_addons.begin(); it != m_addons.end(); ++it)
+  {
+    if ((*it)->ID() == strId)
+    {
+      addon = *it;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool CPeripheralBusAddon::PerformDeviceScan(PeripheralScanResults &results)
+{
+  VECADDONS addons;
+  CAddonMgr::Get().GetAddons(ADDON_PERIPHERALDLL, addons, true);
+
+  {
+    CSingleLock lock(m_critSection);
+
+    PeripheralAddonVector createdAddons;
+    createdAddons.swap(m_addons);
+
+    for (VECADDONS::const_iterator it = addons.begin(); it != addons.end(); ++it)
+    {
+      PeripheralAddonPtr addon = boost::dynamic_pointer_cast<CPeripheralAddon>(*it);
+      if (!addon)
+        continue;
+
+      // If add-on hasn't been created, try to create it now
+      if (std::find(createdAddons.begin(), createdAddons.end(), addon) == createdAddons.end())
+      {
+        if (addon->Create() != ADDON_STATUS_OK)
+        {
+          CAddonDatabase addonDb;
+          if (addonDb.Open())
+            addonDb.DisableAddon(addon->ID());
+          continue;
+        }
+      }
+
+      m_addons.push_back(addon);
+    }
+
+    for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+      (*itAddon)->PerformDeviceScan(results);
+  }
+
+  // Scan during bus initialization must return true or bus gets deleted
+  return true;
+}
+
+void CPeripheralBusAddon::ProcessEvents(void)
+{
+  CSingleLock lock(m_critSection);
+
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    (*itAddon)->ProcessEvents();
+}
+
+void CPeripheralBusAddon::UnregisterRemovedDevices(const PeripheralScanResults &results)
+{
+  CSingleLock lock(m_critSection);
+
+  std::vector<CPeripheral*> removedPeripherals;
+
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    (*itAddon)->UnregisterRemovedDevices(results, removedPeripherals);
+
+  for (unsigned int iDevicePtr = 0; iDevicePtr < removedPeripherals.size(); iDevicePtr++)
+  {
+    CPeripheral *peripheral = removedPeripherals.at(iDevicePtr);
+    m_manager->OnDeviceDeleted(*this, *peripheral);
+    delete peripheral;
+  }
+}
+
+void CPeripheralBusAddon::Register(CPeripheral* peripheral)
+{
+  if (!peripheral)
+    return;
+
+  PeripheralAddonPtr addon;
+  unsigned int       peripheralIndex;
+
+  CSingleLock lock(m_critSection);
+
+  if (SplitLocation(peripheral->Location(), addon, peripheralIndex))
+  {
+    if (addon->Register(peripheralIndex, peripheral))
+      m_manager->OnDeviceAdded(*this, *peripheral);
+  }
+}
+
+void CPeripheralBusAddon::GetFeatures(std::vector<PeripheralFeature> &features) const
+{
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    (*itAddon)->GetFeatures(features);
+}
+
+bool CPeripheralBusAddon::HasFeature(const PeripheralFeature feature) const
+{
+  bool bReturn(false);
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    bReturn = bReturn || (*itAddon)->HasFeature(feature);
+  return bReturn;
+}
+
+CPeripheral *CPeripheralBusAddon::GetPeripheral(const CStdString &strLocation) const
+{
+  CPeripheral*       peripheral(NULL);
+  PeripheralAddonPtr addon;
+  unsigned int       peripheralIndex;
+
+  CSingleLock lock(m_critSection);
+
+  if (SplitLocation(strLocation, addon, peripheralIndex))
+    peripheral = addon->GetPeripheral(peripheralIndex);
+
+  return peripheral;
+}
+
+CPeripheral *CPeripheralBusAddon::GetByPath(const CStdString &strPath) const
+{
+  CSingleLock lock(m_critSection);
+
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+  {
+    CPeripheral* peripheral = (*itAddon)->GetByPath(strPath);
+    if (peripheral)
+      return peripheral;
+  }
+
+  return NULL;
+}
+
+int CPeripheralBusAddon::GetPeripheralsWithFeature(std::vector<CPeripheral *> &results, const PeripheralFeature feature) const
+{
+  int iReturn(0);
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    iReturn += (*itAddon)->GetPeripheralsWithFeature(results, feature);
+  return iReturn;
+}
+
+size_t CPeripheralBusAddon::GetNumberOfPeripherals(void) const
+{
+  size_t iReturn(0);
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    iReturn += (*itAddon)->GetNumberOfPeripherals();
+  return iReturn;
+}
+
+size_t CPeripheralBusAddon::GetNumberOfPeripheralsWithId(const int iVendorId, const int iProductId) const
+{
+  size_t iReturn(0);
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    iReturn += (*itAddon)->GetNumberOfPeripheralsWithId(iVendorId, iProductId);
+  return iReturn;
+}
+
+void CPeripheralBusAddon::GetDirectory(const CStdString &strPath, CFileItemList &items) const
+{
+  CStdString strDevPath;
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    (*itAddon)->GetDirectory(strPath, items);
+}
+
+bool CPeripheralBusAddon::SplitLocation(const std::string& strLocation, PeripheralAddonPtr& addon, unsigned int& peripheralIndex) const
+{
+  std::vector<std::string> parts = StringUtils::Split(strLocation, "/");
+  if (parts.size() == 2)
+  {
+    addon.reset();
+
+    CSingleLock lock(m_critSection);
+
+    const std::string& strAddonId = parts[0];
+    for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    {
+      if ((*itAddon)->ID() == strAddonId)
+      {
+        addon = *itAddon;
+        break;
+      }
+    }
+
+    if (addon)
+    {
+      const char* strJoystickIndex = parts[1].c_str();
+      char* p = NULL;
+      peripheralIndex = strtol(strJoystickIndex, &p, 10);
+      if (strJoystickIndex != p)
+        return true;
+    }
+  }
+  return false;
+}

notspiff/kodi-cmake

xbmc/peripherals/addons/PeripheralAddon.h
@@ -1,0 +1,134 @@
+#pragma once
+/*
+ *      Copyright (C) 2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "addons/AddonDll.h"
+#include "addons/DllPeripheral.h"
+#include "addons/include/xbmc_peripheral_types.h"
+#include "addons/include/xbmc_peripheral_utils.hpp"
+#include "input/joysticks/JoystickTypes.h"
+#include "peripherals/PeripheralTypes.h"
+#include "threads/Thread.h"
+
+#include <memory>
+#include <vector>
+
+class CInputPrimitive;
+class IJoystickInputHandler;
+
+namespace PERIPHERALS
+{
+  class CPeripheral;
+
+  class CPeripheralAddon;
+  typedef std::shared_ptr<CPeripheralAddon> PeripheralAddonPtr;
+  typedef std::vector<PeripheralAddonPtr>   PeripheralAddonVector;
+
+  typedef std::shared_ptr<ADDON::JoystickFeature>         JoystickFeaturePtr;
+  typedef std::map<JoystickFeatureID, JoystickFeaturePtr> JoystickFeatureMap;
+
+  class CPeripheralAddon : public ADDON::CAddonDll<DllPeripheral, PeripheralAddon, PERIPHERAL_PROPERTIES>
+  {
+  public:
+    CPeripheralAddon(const ADDON::AddonProps& props);
+    CPeripheralAddon(const cp_extension_t *ext);
+    virtual ~CPeripheralAddon(void);
+
+    virtual ADDON::AddonPtr GetRunningInstance(void) const;
+
+    /*!
+     * @brief Initialise the instance of this add-on
+     */
+    ADDON_STATUS Create(void);
+
+    /*!
+     * @brief Destroy the instance of this add-on
+     */
+    void Destroy(void);
+
+    bool         Register(unsigned int peripheralIndex, CPeripheral* peripheral);
+    void         UnregisterRemovedDevices(const PeripheralScanResults &results, std::vector<CPeripheral*>& removedPeripherals);
+    void         GetFeatures(std::vector<PeripheralFeature> &features) const;
+    bool         HasFeature(const PeripheralFeature feature) const;
+    CPeripheral* GetPeripheral(unsigned int index) const;
+    CPeripheral* GetByPath(const std::string &strPath) const;
+    int          GetPeripheralsWithFeature(std::vector<CPeripheral*> &results, const PeripheralFeature feature) const;
+    size_t       GetNumberOfPeripherals(void) const;
+    size_t       GetNumberOfPeripheralsWithId(const int iVendorId, const int iProductId) const;
+    void         GetDirectory(const std::string &strPath, CFileItemList &items) const;
+
+    /** @name Peripheral add-on methods */
+    //@{
+    bool PerformDeviceScan(PeripheralScanResults &results);
+    bool ProcessEvents(void);
+    //@}
+
+    /** @name Joystick methods */
+    //@{
+    bool GetJoystickFeatures(unsigned int index, JoystickFeatureMap& features);
+    //@}
+
+    static const char          *ToString(PERIPHERAL_ERROR error);
+
+  protected:
+    /*!
+     * @brief Request the API version from the add-on, and check if it's compatible
+     * @return True when compatible, false otherwise.
+     */
+    virtual bool CheckAPIVersion(void);
+
+  private:
+    static JoystickFeatureID    ToJoystickID(JOYSTICK_FEATURE_ID id);
+    static JOYSTICK_FEATURE_ID ToFeatureID(JoystickFeatureID id);
+    static HatDirection        ToHatDirection(JOYSTICK_STATE_HAT state);
+
+    /*!
+     * @brief Resets all class members to their defaults. Called by the constructors
+     */
+    void ResetProperties(void);
+
+    bool GetAddonProperties(void);
+
+    /*!
+     * @brief Checks whether the provided API version is compatible with XBMC
+     * @param minVersion The add-on's XBMC_PERIPHERAL_MIN_API_VERSION version
+     * @param version The add-on's XBMC_PERIPHERAL_API_VERSION version
+     * @return True when compatible, false otherwise
+     */
+    static bool IsCompatibleAPIVersion(const ADDON::AddonVersion &minVersion, const ADDON::AddonVersion &version);
+
+    bool LogError(const PERIPHERAL_ERROR error, const char *strMethod) const;
+    void LogException(const std::exception &e, const char *strFunctionName) const;
+
+    /* stored strings to make sure const char* members in PERIPHERAL_PROPERTIES stay valid */
+    std::string         m_strUserPath;    /*!< @brief translated path to the user profile */
+    std::string         m_strClientPath;  /*!< @brief translated path to this add-on */
+
+    /* add-on properties */
+    ADDON::AddonVersion m_apiVersion;
+    bool                m_bProvidesJoysticks;
+
+    /* peripherals */
+    std::map<unsigned int, CPeripheral*>  m_peripherals;
+
+    /* synchronization */
+    CCriticalSection    m_critSection;
+  };
+}

xbmc/peripherals/bus/PeripheralBusAddon.cpp
@@ -1,0 +1,246 @@
+/*
+ *      Copyright (C) 2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "PeripheralBusAddon.h"
+#include "addons/AddonDatabase.h"
+#include "addons/AddonManager.h"
+#include "peripherals/Peripherals.h"
+#include "threads/SingleLock.h"
+#include "utils/log.h"
+
+#include <algorithm>
+#include <memory>
+
+using namespace ADDON;
+using namespace PERIPHERALS;
+
+CPeripheralBusAddon::CPeripheralBusAddon(CPeripherals *manager) :
+    CPeripheralBus("PeripBusAddon", manager, PERIPHERAL_BUS_ADDON)
+{
+}
+
+CPeripheralBusAddon::~CPeripheralBusAddon(void)
+{
+}
+
+bool CPeripheralBusAddon::GetAddon(const std::string &strId, AddonPtr &addon) const
+{
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator it = m_addons.begin(); it != m_addons.end(); ++it)
+  {
+    if ((*it)->ID() == strId)
+    {
+      addon = *it;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool CPeripheralBusAddon::PerformDeviceScan(PeripheralScanResults &results)
+{
+  VECADDONS addons;
+  CAddonMgr::Get().GetAddons(ADDON_PERIPHERALDLL, addons, true);
+
+  {
+    CSingleLock lock(m_critSection);
+
+    PeripheralAddonVector createdAddons;
+    createdAddons.swap(m_addons);
+
+    for (VECADDONS::const_iterator it = addons.begin(); it != addons.end(); ++it)
+    {
+      PeripheralAddonPtr addon = std::dynamic_pointer_cast<CPeripheralAddon>(*it);
+      if (!addon)
+        continue;
+
+      // If add-on hasn't been created, try to create it now
+      if (std::find(createdAddons.begin(), createdAddons.end(), addon) == createdAddons.end())
+      {
+        if (addon->Create() != ADDON_STATUS_OK)
+        {
+          CAddonDatabase addonDb;
+          if (addonDb.Open())
+            addonDb.DisableAddon(addon->ID());
+          continue;
+        }
+      }
+
+      m_addons.push_back(addon);
+    }
+
+    for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+      (*itAddon)->PerformDeviceScan(results);
+  }
+
+  // Scan during bus initialization must return true or bus gets deleted
+  return true;
+}
+
+void CPeripheralBusAddon::ProcessEvents(void)
+{
+  CSingleLock lock(m_critSection);
+
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    (*itAddon)->ProcessEvents();
+}
+
+void CPeripheralBusAddon::UnregisterRemovedDevices(const PeripheralScanResults &results)
+{
+  CSingleLock lock(m_critSection);
+
+  std::vector<CPeripheral*> removedPeripherals;
+
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    (*itAddon)->UnregisterRemovedDevices(results, removedPeripherals);
+
+  for (unsigned int iDevicePtr = 0; iDevicePtr < removedPeripherals.size(); iDevicePtr++)
+  {
+    CPeripheral *peripheral = removedPeripherals.at(iDevicePtr);
+    m_manager->OnDeviceDeleted(*this, *peripheral);
+    delete peripheral;
+  }
+}
+
+void CPeripheralBusAddon::Register(CPeripheral* peripheral)
+{
+  if (!peripheral)
+    return;
+
+  PeripheralAddonPtr addon;
+  unsigned int       peripheralIndex;
+
+  CSingleLock lock(m_critSection);
+
+  if (SplitLocation(peripheral->Location(), addon, peripheralIndex))
+  {
+    if (addon->Register(peripheralIndex, peripheral))
+      m_manager->OnDeviceAdded(*this, *peripheral);
+  }
+}
+
+void CPeripheralBusAddon::GetFeatures(std::vector<PeripheralFeature> &features) const
+{
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    (*itAddon)->GetFeatures(features);
+}
+
+bool CPeripheralBusAddon::HasFeature(const PeripheralFeature feature) const
+{
+  bool bReturn(false);
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    bReturn = bReturn || (*itAddon)->HasFeature(feature);
+  return bReturn;
+}
+
+CPeripheral *CPeripheralBusAddon::GetPeripheral(const std::string &strLocation) const
+{
+  CPeripheral*       peripheral(NULL);
+  PeripheralAddonPtr addon;
+  unsigned int       peripheralIndex;
+
+  CSingleLock lock(m_critSection);
+
+  if (SplitLocation(strLocation, addon, peripheralIndex))
+    peripheral = addon->GetPeripheral(peripheralIndex);
+
+  return peripheral;
+}
+
+CPeripheral *CPeripheralBusAddon::GetByPath(const std::string &strPath) const
+{
+  CSingleLock lock(m_critSection);
+
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+  {
+    CPeripheral* peripheral = (*itAddon)->GetByPath(strPath);
+    if (peripheral)
+      return peripheral;
+  }
+
+  return NULL;
+}
+
+int CPeripheralBusAddon::GetPeripheralsWithFeature(std::vector<CPeripheral *> &results, const PeripheralFeature feature) const
+{
+  int iReturn(0);
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    iReturn += (*itAddon)->GetPeripheralsWithFeature(results, feature);
+  return iReturn;
+}
+
+size_t CPeripheralBusAddon::GetNumberOfPeripherals(void) const
+{
+  size_t iReturn(0);
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    iReturn += (*itAddon)->GetNumberOfPeripherals();
+  return iReturn;
+}
+
+size_t CPeripheralBusAddon::GetNumberOfPeripheralsWithId(const int iVendorId, const int iProductId) const
+{
+  size_t iReturn(0);
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    iReturn += (*itAddon)->GetNumberOfPeripheralsWithId(iVendorId, iProductId);
+  return iReturn;
+}
+
+void CPeripheralBusAddon::GetDirectory(const std::string &strPath, CFileItemList &items) const
+{
+  CSingleLock lock(m_critSection);
+  for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    (*itAddon)->GetDirectory(strPath, items);
+}
+
+bool CPeripheralBusAddon::SplitLocation(const std::string& strLocation, PeripheralAddonPtr& addon, unsigned int& peripheralIndex) const
+{
+  std::vector<std::string> parts = StringUtils::Split(strLocation, "/");
+  if (parts.size() == 2)
+  {
+    addon.reset();
+
+    CSingleLock lock(m_critSection);
+
+    const std::string& strAddonId = parts[0];
+    for (PeripheralAddonVector::const_iterator itAddon = m_addons.begin(); itAddon != m_addons.end(); ++itAddon)
+    {
+      if ((*itAddon)->ID() == strAddonId)
+      {
+        addon = *itAddon;
+        break;
+      }
+    }
+
+    if (addon)
+    {
+      const char* strJoystickIndex = parts[1].c_str();
+      char* p = NULL;
+      peripheralIndex = strtol(strJoystickIndex, &p, 10);
+      if (strJoystickIndex != p)
+        return true;
+    }
+  }
+  return false;
+}