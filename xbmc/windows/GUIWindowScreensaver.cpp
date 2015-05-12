70d1fcde7dcabb14abfd5e3888f55b7ca069dbbb
Arne Morten Kvarving <cptspiff@gmail.com>
2015-05-12 16:43:04 +0200

    added: allow passing screensaver to use a parameter to window

--- xbmc/windows/GUIWindowScreensaver.cpp
+++ xbmc/windows/GUIWindowScreensaver.cpp
@@ -133,6 +133,9 @@ bool CGUIWindowScreensaver::OnMessage(CGUIMessage& message)
 #ifdef HAS_SCREENSAVER
       assert(!m_addon);
       m_bInitialized = false;
+      std::string addonID = message.GetStringParam(0);
+      if (addonID.empty())
+        addonID = CSettings::Get().GetString("screensaver.mode");
 
       m_addon.reset();
       // Setup new screensaver instance
