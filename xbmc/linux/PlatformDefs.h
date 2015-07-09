7fec5642317151a1523b9fee89a9c9c68fbeb9df
fritsch <Peter.Fruehberger@gmail.com>
2015-07-09 22:07:35 +0200

    PlatformDefs: Fix compilation issue on i386 (revisit me later)

--- xbmc/linux/PlatformDefs.h
+++ xbmc/linux/PlatformDefs.h
@@ -164,7 +164,7 @@
 #if defined(__x86_64__) || defined(__powerpc__) || defined(__ppc__) || defined (__arm__) || defined(__mips__) // should this be powerpc64 only?
 #define __stdcall
 #else /* !__x86_64__ */
-#define __stdcall   __attribute__((__stdcall__))
+#define __stdcall
 #endif /* __x86_64__ */
 #define __cdecl
 #define WINBASEAPI
