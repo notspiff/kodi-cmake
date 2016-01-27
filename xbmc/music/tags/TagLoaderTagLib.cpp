fritsch/xbmc-cmake

added: POC mp4 movie/episode tag support

xbmc/music/tags/TagLoaderTagLib.cpp
@@ -46,6 +46,7 @@
 #include "utils/CharsetConverter.h"
 #include "utils/Base64.h"
 #include "settings/AdvancedSettings.h"
+#include "video/VideoInfoTag.h"
 
 using namespace std;
 using namespace TagLib;
@@ -252,6 +253,45 @@ bool CTagLoaderTagLib::Load(const CStdString& strFileName, CMusicInfoTag& tag, c
   if (!tag.GetTitle().empty() || !tag.GetArtist().empty() || !tag.GetAlbum().empty())
     tag.SetLoaded();
   tag.SetURL(strFileName);
+
+  delete file;
+  delete stream;
+  
+  return true;
+}
+
+bool CTagLoaderTagLib::Load(const CStdString& strFileName, CVideoInfoTag& tag)
+{  
+  CStdString strExtension = URIUtils::GetExtension(strFileName);
+  StringUtils::ToLower(strExtension);
+  StringUtils::TrimLeft(strExtension, ".");
+
+  TagLibVFSStream*           stream = new TagLibVFSStream(strFileName, true);
+  if (!stream)
+  {
+    CLog::Log(LOGERROR, "could not create TagLib VFS stream for: %s", strFileName.c_str());
+    return false;
+  }
+  
+  TagLib::File*              file = NULL;
+  TagLib::MP4::File*         mp4File = NULL;
+
+  if (strExtension == "mp4")
+    file = mp4File = new MP4::File(stream);
+
+  if (file && !file->isOpen())
+  {
+    delete file;
+    delete stream;
+    CLog::Log(LOGDEBUG, "file could not be opened for tag reading");
+    return false;
+  }
+  if (!file)
+    return true;
+
+  MP4::Tag *mp4 = NULL;
+  mp4 = mp4File->tag();
+  ParseMP4Tag(mp4, tag);
 
   delete file;
   delete stream;
@@ -669,6 +709,33 @@ bool CTagLoaderTagLib::ParseMP4Tag(MP4::Tag *mp4, EmbeddedArt *art, CMusicInfoTa
         break; // one is enough
       }
     }
+  }
+
+  return true;
+}
+
+bool CTagLoaderTagLib::ParseMP4Tag(MP4::Tag *mp4, CVideoInfoTag& tag)
+{
+  if (!mp4)
+    return false;
+
+  MP4::ItemListMap& itemListMap = mp4->itemListMap();
+  for (MP4::ItemListMap::ConstIterator it = itemListMap.begin(); it != itemListMap.end(); ++it)
+  {
+    if (it->first == "\251nam")
+      tag.m_strTitle = it->second.toStringList().front().to8Bit(true);
+    else if (it->first == "\251vsh")
+      tag.m_strShowTitle = it->second.toStringList().front().to8Bit(true);
+    else if (it->first == "\251gen")
+      tag.m_genre = StringListToVectorString(it->second.toStringList());
+    else if (it->first == "\251cmt")
+      tag.m_strPlot = it->second.toStringList().front().to8Bit(true);
+    else if (it->first == "\251desc")
+      tag.m_strPlotOutline = it->second.toStringList().front().to8Bit(true);
+    else if (it->first == "tvsn")
+      tag.m_iSeason = it->second.toIntPair().first;
+    else if (it->first == "tves")
+      tag.m_iEpisode = it->second.toIntPair().first;
   }
 
   return true;

notspiff/kodi-cmake

xbmc/music/tags/TagLoaderTagLib.cpp
@@ -49,6 +49,7 @@
 #include "utils/CharsetConverter.h"
 #include "utils/Base64.h"
 #include "settings/AdvancedSettings.h"
+#include "video/VideoInfoTag.h"
 
 using namespace TagLib;
 using namespace MUSIC_INFO;
@@ -92,6 +93,45 @@ bool CTagLoaderTagLib::Load(const std::string& strFileName, MUSIC_INFO::CMusicIn
   return Load(strFileName, tag, "", art);
 }
 
+
+bool CTagLoaderTagLib::Load(const std::string& strFileName, CVideoInfoTag& tag)
+{
+  std::string strExtension = URIUtils::GetExtension(strFileName);
+  StringUtils::ToLower(strExtension);
+  StringUtils::TrimLeft(strExtension, ".");
+
+  TagLibVFSStream*           stream = new TagLibVFSStream(strFileName, true);
+  if (!stream)
+  {
+    CLog::Log(LOGERROR, "could not create TagLib VFS stream for: %s", strFileName.c_str());
+    return false;
+  }
+
+  TagLib::File*              file = NULL;
+  TagLib::MP4::File*         mp4File = NULL;
+
+  if (strExtension == "mp4")
+    file = mp4File = new MP4::File(stream);
+
+  if (file && !file->isOpen())
+  {
+    delete file;
+    delete stream;
+    CLog::Log(LOGDEBUG, "file could not be opened for tag reading");
+    return false;
+  }
+  if (!file)
+    return true;
+
+  MP4::Tag *mp4 = NULL;
+  mp4 = mp4File->tag();
+  ParseTag(mp4, tag);
+
+  delete file;
+  delete stream;
+
+  return true;
+}
 
 template<>
 bool CTagLoaderTagLib::ParseTag(ASF::Tag *asf, EmbeddedArt *art, CMusicInfoTag& tag)