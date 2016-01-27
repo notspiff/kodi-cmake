biopendent/xbmc-cmake

add support for vfs add-ons

xbmc/addons/Addon.cpp
@@ -89,6 +89,7 @@ static const TypeMapping types[] =
    {"xbmc.addon.executable",             ADDON_EXECUTABLE,           1043, "DefaultAddonProgram.png" },
    {"xbmc.audioencoder",                 ADDON_AUDIOENCODER,         200,  "DefaultAddonAudioEncoder.png" },
    {"xbmc.audiodecoder",                 ADDON_AUDIODECODER,         201,  "DefaultAddonAudioDecoder.png" },
+   {"xbmc.vfs",                          ADDON_VFS,                  204,  "DefaultAddonVfs.png" },
    {"xbmc.service",                      ADDON_SERVICE,             24018, "DefaultAddonService.png" }};
 
 const CStdString TranslateType(const ADDON::TYPE &type, bool pretty/*=false*/)
@@ -395,6 +396,7 @@ void CAddon::BuildLibName(const cp_extension_t *extension)
       case ADDON_REPOSITORY:
       case ADDON_AUDIODECODER:
       case ADDON_AUDIOENCODER:
+      case ADDON_VFS:
         {
           CStdString temp = CAddonMgr::Get().GetExtValue(extension->configuration, "@library");
           m_strLibName = temp;

xbmc/filesystem/FileDirectoryFactory.cpp
@@ -46,6 +46,7 @@
 #include "utils/StringUtils.h"
 #include "addons/AddonManager.h"
 #include "addons/AudioDecoder.h"
+#include "addons/VFSEntry.h"
 
 using namespace ADDON;
 using namespace XFILE;
@@ -81,6 +82,35 @@ IFileDirectory* CFileDirectoryFactory::Create(const CStdString& strPath, CFileIt
       if (result->ContainsFiles(strPath))
         return result;
       delete result;
+      return NULL;
+    }
+  }
+
+  CAddonMgr::Get().GetAddons(ADDON_VFS, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CVFSEntry> dec(boost::static_pointer_cast<CVFSEntry>(codecs[i]));
+    if (!strExtension.empty() && dec->HasFileDirectories() &&
+        dec->GetExtensions().find(strExtension) != std::string::npos)
+    {
+      CVFSEntryIFileDirectoryWrapper* wrap = new CVFSEntryIFileDirectoryWrapper(CVFSEntryManager::Get().GetAddon(dec->ID()));
+      if (wrap->ContainsFiles(strPath))
+      {
+        if (wrap->m_items.Size() == 1)
+        {
+          // one STORED file - collapse it down
+          *pItem = *wrap->m_items[0];
+        }
+        else
+        { // compressed or more than one file -> create a dir
+          pItem->SetPath(wrap->m_items.GetPath());
+          return wrap;
+        }
+      }
+      else
+        pItem->m_bIsFolder = true;
+
+      delete wrap;
       return NULL;
     }
   }

notspiff/kodi-cmake

xbmc/addons/Addon.cpp
@@ -102,6 +102,7 @@ static const TypeMapping types[] =
    {"kodi.resource.images",              ADDON_RESOURCE_IMAGES,     24035, "DefaultAddonImages.png" },
    {"kodi.resource.language",            ADDON_RESOURCE_LANGUAGE,   24026, "DefaultAddonLanguage.png" },
    {"kodi.resource.uisounds",            ADDON_RESOURCE_UISOUNDS,   24006, "DefaultAddonUISounds.png" },
+   {"kodi.vfs",                          ADDON_VFS,                   204,  "DefaultAddonVfs.png" },
    {"kodi.adsp",                         ADDON_ADSPDLL,             24135, "DefaultAddonAudioDSP.png" },
   };
 
@@ -412,6 +413,8 @@ void CAddon::BuildLibName(const cp_extension_t *extension)
       case ADDON_SERVICE:
       case ADDON_REPOSITORY:
       case ADDON_CONTEXT_ITEM:
+      case ADDON_AUDIODECODER:
+      case ADDON_VFS:
         m_strLibName = CAddonMgr::GetInstance().GetExtValue(extension->configuration, "@library");
         break;
       default:

xbmc/filesystem/FileDirectoryFactory.cpp
@@ -45,6 +45,7 @@
 #include "URL.h"
 #include "addons/AddonManager.h"
 #include "addons/AudioDecoder.h"
+#include "addons/VFSEntry.h"
 
 using namespace ADDON;
 using namespace XFILE;
@@ -82,7 +83,35 @@ IFileDirectory* CFileDirectoryFactory::Create(const CURL& url, CFileItem* pItem,
   }
 
 #ifdef HAS_FILESYSTEM
-  
+  CAddonMgr::GetInstance().GetAddons(codecs, ADDON_VFS);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    std::shared_ptr<CVFSEntry> dec(std::static_pointer_cast<CVFSEntry>(codecs[i]));
+    if (!strExtension.empty() && dec->HasFileDirectories() &&
+        dec->GetExtensions().find(strExtension) != std::string::npos)
+    {
+      CVFSEntryIFileDirectoryWrapper* wrap = new CVFSEntryIFileDirectoryWrapper(CVFSEntryManager::Get().GetAddon(dec->ID()));
+      if (wrap->ContainsFiles(url))
+      {
+        if (wrap->m_items.Size() == 1)
+        {
+          // one STORED file - collapse it down
+          *pItem = *wrap->m_items[0];
+        }
+        else
+        { // compressed or more than one file -> create a dir
+          pItem->SetPath(wrap->m_items.GetPath());
+          return wrap;
+        }
+      }
+      else
+        pItem->m_bIsFolder = true;
+
+      delete wrap;
+      return NULL;
+    }
+  }
+
   if (pItem->IsRSS())
     return new CRSSDirectory();
 